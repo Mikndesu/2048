4d0e964479958998443a39a77a91db21d8d58776
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2023-10-05 00:22:01 +0900

    add colour enum

--- src/headers/colour.hpp
+++ src/headers/colour.hpp
@@ -1,4 +1,14 @@
 namespace Utility2048 {
 enum class Colour {
+    WHITE,
+    GREEN,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    RED,
+    YELLOW
 };
+int getIntValue(Colour colour) {
+    return static_cast<int>(colour);
+}
 }  // namespace Utility2048
