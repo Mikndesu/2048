703152214127ac2890992d3920251e2e8f07982f
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2023-10-15 22:27:52 +0900

    initialise two tiles on startup

--- src/game_board_state.rs
+++ src/game_board_state.rs
@@ -1,14 +1,22 @@
+use rand::rngs::ThreadRng;
+
 mod game_board_state_internal;
 
 pub struct GameBoardState {
     game_state: [[i32; 4]; 4],
+    randomiser: ThreadRng,
 }
 
 impl GameBoardState {
     pub fn new() -> Self {
-        Self {
+        let mut instance = Self {
             game_state: [[0; 4]; 4],
+            randomiser: rand::thread_rng(),
+        };
+        for _ in 0..=1 {
+            instance.initialise_tile();
         }
+        instance
     }
     pub fn get_state(&self) -> [[i32; 4]; 4] {
         self.game_state
--- src/game_board_state/game_board_state_internal.rs
+++ src/game_board_state/game_board_state_internal.rs
@@ -1,22 +1,34 @@
 use super::GameBoardState;
+use rand::Rng;
 
 impl GameBoardState {
-    pub(crate) fn initialise_certain_tile(&mut self, y: i32, x: i32) {
+    pub(crate) fn initialise_tile(&mut self) {
+        let y = self.random_coordinate();
+        let x = self.random_coordinate();
         if self.is_tile_initialised(y, x) {
-            self.initialise_certain_tile(y, x)
+            self.initialise_tile()
         }
-        self.update_certain_tile_internal(y, x, self.generate_new_tile_value());
+        let new_value = self.generate_new_tile_value();
+        self.update_certain_tile_internal(y, x, new_value);
     }
 
     pub(crate) fn is_tile_initialised(&self, y: i32, x: i32) -> bool {
         self.game_state[y as usize][x as usize] != 0
     }
 
-    pub(crate) fn generate_new_tile_value(&self) -> i32 {
-        2
+    pub(crate) fn generate_new_tile_value(&mut self) -> i32 {
+        if self.randomiser.gen_range(1..10) == 1 {
+            4
+        } else {
+            2
+        }
     }
 
-    pub(crate) fn update_certain_tile_internal(&mut self, y: i32, x: i32, value: i32) {
+    fn update_certain_tile_internal(&mut self, y: i32, x: i32, value: i32) {
         self.game_state[y as usize][x as usize] = value;
+    }
+
+    fn random_coordinate(&mut self) -> i32 {
+        self.randomiser.gen_range(0..3)
     }
 }
